bcfd98a50282eab75128995852d673fe32544bbd
Radhikacompalies <radhika@123gmail.com>
2023-12-05 19:21:39 +0530

    Initial commit - <commit_message>

---
+++ Betnride/src/main/java/com/example/Betnride/Averageapplications.java
@@ -0,0 +1,5 @@
+package com.example.Betnride;
+
+public class Averageapplications {
+
+}
---
+++ Betnride/src/main/java/com/example/Betnride/BetnrideApplication.java
@@ -0,0 +1,13 @@
+package com.example.Betnride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetnrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetnrideApplication.class, args);
+	}
+
+}
